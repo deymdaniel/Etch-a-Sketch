a5202287485119cd2844a1b35da8fc8ae95ad59e
deymdaniel <danielbiacaniii@gmail.com>
2024-01-12 16:48:54 +0800

    Added butto n functionality

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-let gridSize = 24;
-const gridContainer = document.querySelector("#container");
+const button = document.querySelector(".canvasBtn");
+button.addEventListener("click", function () {
+  gridSize = prompt("Enter grid size");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const grid = document.createElement("div");
-  grid.setAttribute("id", "black");
-  grid.style =
-    "border: 1px solid; width: " +
-    (500 - 2 * gridSize) / gridSize +
-    "px; height: " +
-    (500 - 2 * gridSize) / gridSize +
-    "px; border-radius: 4px";
-  // grid.style = "width: " + 10 + "px";
+  const gridContainer = document.querySelector("#container");
 
-  gridContainer.appendChild(grid);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const grid = document.createElement("div");
+    grid.setAttribute("id", "black");
+    grid.style =
+      "border: 1px solid; width: " +
+      (500 - 2 * gridSize) / gridSize +
+      "px; height: " +
+      (500 - 2 * gridSize) / gridSize +
+      "px; border-radius: 4px";
+    // grid.style = "width: " + 10 + "px";
 
-  grid.addEventListener("mouseover", function () {
-    grid.style.backgroundColor = "black";
-  });
-}
+    gridContainer.appendChild(grid);
+
+    grid.addEventListener("mouseover", function () {
+      grid.style.backgroundColor = "black";
+    });
+  }
+});
 
 // const black = document.querySelector("#black");
 
