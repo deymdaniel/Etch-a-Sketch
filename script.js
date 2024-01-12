e48574cbcedbcf451bf81bbf3029ac44764aa51a
deymdaniel <danielbiacaniii@gmail.com>
2024-01-12 15:02:16 +0800

    Added drawing functionality

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let gridSize = 16;
+let gridSize = 24;
 const gridContainer = document.querySelector("#container");
 
 for (let i = 0; i < gridSize * gridSize; i++) {
   const grid = document.createElement("div");
+  grid.setAttribute("id", "black");
   grid.style =
     "border: 1px solid; width: " +
     (500 - 2 * gridSize) / gridSize +
@@ -12,4 +13,14 @@ for (let i = 0; i < gridSize * gridSize; i++) {
   // grid.style = "width: " + 10 + "px";
 
   gridContainer.appendChild(grid);
+
+  grid.addEventListener("mouseover", function () {
+    grid.style.backgroundColor = "black";
+  });
 }
+
+// const black = document.querySelector("#black");
+
+// black.addEventListener("mouseover", function () {
+//   black.style = "background-color: black";
+// });
