46c4249e684e00fdba5d27f71f0af0cfb14f44d8
deymdaniel <danielbiacaniii@gmail.com>
2024-01-14 14:05:40 +0800

    Fixed canvas size button to remove old grid

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const button = document.querySelector(".canvasBtn");
 button.addEventListener("click", function () {
+  const gridContainer = document.querySelector("#container");
+  gridContainer.innerHTML = "";
+
   gridSize = prompt("Enter grid size");
 
-  const gridContainer = document.querySelector("#container");
+  if (gridSize > 64 || gridSize < 0) {
+    alert("Enter a valid number");
+  } else {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      const grid = document.createElement("div");
 
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const grid = document.createElement("div");
-    grid.setAttribute("id", "black");
-    grid.style =
-      "border: 1px solid; width: " +
-      (500 - 2 * gridSize) / gridSize +
-      "px; height: " +
-      (500 - 2 * gridSize) / gridSize +
-      "px; border-radius: 4px";
-    // grid.style = "width: " + 10 + "px";
+      grid.setAttribute("id", "black");
+      grid.style =
+        "border: 1px solid; width: " +
+        (500 - 2 * gridSize) / gridSize +
+        "px; height: " +
+        (500 - 2 * gridSize) / gridSize +
+        "px; border-radius: 4px";
 
-    gridContainer.appendChild(grid);
+      gridContainer.appendChild(grid);
 
-    grid.addEventListener("mouseover", function () {
-      grid.style.backgroundColor = "black";
-    });
+      grid.addEventListener("mouseover", function () {
+        grid.style.backgroundColor = "black";
+      });
+    }
   }
 });
-
-// const black = document.querySelector("#black");
-
-// black.addEventListener("mouseover", function () {
-//   black.style = "background-color: black";
-// });
